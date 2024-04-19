a62b23c3b0dd725827d8c8c097177e8883632926
joseph <michealkinuthia.m.k@gmail.com>
2024-04-19 16:24:59 +0300

    success

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import necessary components
 import './App.css';
 import { Login } from "./Login";
 import { Register } from "./Register";
+import Home from "./Home"; // Import Home component
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
+  // const [currentForm, setCurrentForm] = useState('login');
 
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
+  // const toggleForm = (formName) => {
+  //   setCurrentForm(formName);
+  // }
 
   return (
-    <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios"; // Import Axios for making HTTP requests
+import { useNavigate } from "react-router-dom"; // Import useHistory
+
+// import { Button } from "react-bootstrap";
+// import { Register } from "./Register";
+
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const history = useNavigate(); // Initialize useHistory
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -11,6 +17,8 @@ export const Login = (props) => {
             // Send a POST request to your backend endpoint for user authentication
             const response = await axios.post('/api/login', { email, password });
             console.log(response.data); // Assuming your backend sends back some data upon successful login
+            // Redirect to homepage after successful login
+            history.push("/home");
             // Optionally, you can redirect the user to another page upon successful login
         } catch (error) {
             console.error("Login failed:", error);
