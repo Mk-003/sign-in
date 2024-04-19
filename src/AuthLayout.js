fe7c6443ce2282fb9e234c7a4aed93ca0697bbc9
joseph <michealkinuthia.m.k@gmail.com>
2024-04-19 18:09:30 +0300

    comie!

--- src/AuthLayout.js
+++ src/AuthLayout.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const AuthLayout = ({ children }) => {
   return (
     <div>
-      <h1>Welcome to the Auth Pages</h1>
+      <h1>WaWAwa</h1>
+      <p>mAAAi LAWD IwoNDOR</p>
       <nav>
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
